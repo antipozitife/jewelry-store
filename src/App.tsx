f5f2c6a3d6d900188a149b9ab2d84e4eddd96e90
Алина Гарипова <alinagaripova@MacBook-Air-Alina.local>
2025-06-17 23:19:20 +0300

    добавила неработающую главную страницу

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Main from './parts/main';
 
 function App() {
   return (
     <Router>
-      
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </Router>
   );
 }
